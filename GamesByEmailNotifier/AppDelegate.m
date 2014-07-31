8d928266e72c8cf0d2483bab074578c91e92d965
Bill Bradley <bill@bill-bradley.com>
2014-07-31 16:18:33 -0400

    Added status bar icon, and launches to your turns

--- GamesByEmailNotifier/AppDelegate.m
+++ GamesByEmailNotifier/AppDelegate.m
@@ -9,10 +9,37 @@
 #import "AppDelegate.h"
 
 @implementation AppDelegate
+@synthesize statusBar = _statusBar;
+
+- (void) awakeFromNib {
+    self.statusBar = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    
+    // self.statusBar.title = @"GamesByEmail Turn Notifier";
+    self.statusBar.image = [NSImage imageNamed:@"pawn_19_19.png"];
+    
+    self.statusBar.menu = self.statusMenu;
+    self.statusBar.highlightMode = YES;
+    
+}
+
+- (IBAction)checkForTurns:(id)sender {
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert addButtonWithTitle:@"OK"];
+    [alert setMessageText:@"Checking for turns."];
+    [alert setAlertStyle:NSWarningAlertStyle];
+    
+    if ([alert runModal] == NSAlertFirstButtonReturn) {
+        // OK clicked, delete the record
+    }
+}
+
+- (IBAction)viewMyGames:(id)sender {
+    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"http://www.gamesbyemail.com/User/MyGames"]];
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+
 }
 
 @end
